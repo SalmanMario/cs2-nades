d384b8714909bee2f5482c19c75e0078b49029d7
mario <mario@agaweb.it>
2026-05-07 09:04:28 +0200

    Create Page v2 Utilities added missing files

---
+++ app/Http/Requests/UtilityRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enum\MovementEnum;
+use App\Enum\TechniqueEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UtilityRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'grenade_name' => 'required|string|max:255',
+            'team_type_id' => 'required|exists:teams,id',
+            'utility_type_id' => 'required|exists:utility_types,id',
+            'movement_type' => ['required', Rule::enum(MovementEnum::class)],
+            'technique_type' => ['required', Rule::enum(TechniqueEnum::class)],
+            'title_from' => 'required|string|max:255',
+            'title_to' => 'required|string|max:255',
+            'start_coords_x' => 'required_if:existing_start_coords_x,null|string',
+            'start_coords_y' => 'required_if:existing_start_coords_y,null|string',
+            'end_coords_x' => 'required_if:existing_end_coords_x,null|string',
+            'end_coords_y' => 'required_if:existing_end_coords_y,null|string',
+            'existing_start_coords_x' => 'nullable|string',
+            'existing_start_coords_y' => 'nullable|string',
+            'existing_end_coords_x' => 'nullable|string',
+            'existing_end_coords_y' => 'nullable|string',
+            // TODO REQUIRED BOTH IMAGE AND VIDEO
+            'image_lineup' => 'nullable|array|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'video_lineup' => 'nullable|array|mimes:mp4,avi,mov,wmv,flv,mkv|max:8192',
+        ];
+    }
+}
---
+++ app/Http/Resources/UtilityResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UtilityResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'grenade_name' => $this->grenade_name,
+            'team_type' => $this->team->name,
+            'team_image' => $this->team->image,
+            'utility_type' => $this->utilityCoordinates?->utility_type?->name,
+            'utility_image' => $this->utilityCoordinates?->utility_type?->image,
+            'start_coords_x' => $this->utilityCoordinates?->start_utility_coordinates?->x,
+            'start_coords_y' => $this->utilityCoordinates?->start_utility_coordinates?->y,
+            'start_coords_title_from' => $this->utilityCoordinates?->start_utility_coordinates?->title_from,
+            'end_coords_x' => $this->utilityCoordinates?->end_utility_coordinates?->x,
+            'end_coords_y' => $this->utilityCoordinates?->end_utility_coordinates?->y,
+            'end_coords_title_to' => $this->utilityCoordinates?->end_utility_coordinates?->title_to,
+            'technique_type' => $this->technique_type,
+            'movement_type' => $this->movement_type,
+        ];
+    }
+}
